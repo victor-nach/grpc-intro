f257e4b33090738133c2fb094be342d84ad7ada0
victor <viheanaco@gmail.com>
2020-01-30 20:16:30 +0100

    implement unary client to call GreetService #5

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
-	"google.golang.org/grpc"
-	"github.com/victor-nach/grpc-intro/greet/greetpb"
 
+	"github.com/victor-nach/grpc-intro/greet/greetpb"
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -20,7 +21,28 @@ func main() {
 
 	defer conn.Close()
 
-	// create a client
+	// create a client for the greet service
 	c := greetpb.NewGreetServiceClient(conn)
-	fmt.Printf("Created client: %f", c)
+	// fmt.Printf("Created client: %f", c)
+	doUnary(c)
+
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do Unary RPC....")
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Iheanacho",
+			LastName:  "Victor",
+		},
+	}
+
+	// the created client has access to the calls defined in the protobuf file
+	// so here we do a rpc call to the function as defined in the service by passing in a greetRequest type
+	// then we get a GreetResponse type as defined in the protobuf file
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling greet RPC: %v\n", err)
+	}
+	log.Printf("Response from Greet: %v", res.Result)
 }
