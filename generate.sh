0b7a233268b35aa18c4e80bae017910b8ab41d31
victor <viheanaco@gmail.com>
2020-01-30 20:46:47 +0100

    set up protobuf for calculator service #6

--- generate.sh
+++ generate.sh
@@ -1,3 +1,7 @@
 #!bin/bash
 
+# greet service
 protoc greet/greetpb/greet.proto --go_out=plugins=grpc:.
+
+# calculator service
+protoc calculator/calculatorpb/calculator.proto --go_out=plugins=grpc:.
